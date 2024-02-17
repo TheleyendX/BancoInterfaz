36400f1a3f5ffde87baa1bd49818c0a9d323cb59
TheleyendX <jorge.enriquez233274@potros.itson.edu.mx>
2024-02-16 23:24:52 -0700

    se agrega un repositorio

---
+++ src/main/java/com/mycompany/bancointerfaz/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bancointerfaz;
+
+/**
+ *
+ * @author USER
+ */
+public class Main {
+    public static void main(String[] args) {
+        InicioSesion i=new InicioSesion();
+        i.show();
+    }
+}
